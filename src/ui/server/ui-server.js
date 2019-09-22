d7905a227194f568e896a7b8ccadea7aeada881b
Muxa89 <muxa.89@inbox.ru>
2019-09-22 15:25:33 +0400

    Finished 3.1 page.

--- src/ui/server/ui-server.js
+++ src/ui/server/ui-server.js
@@ -29,5 +29,6 @@ app.get('/1440/1.1', (req, res) => res.render(resolve(UI_SRC_DIR, TEMPLATES_DIR,
 app.get('/1440/1.5', (req, res) => res.render(resolve(UI_SRC_DIR, TEMPLATES_DIR, './1440/1.5.pug'), require('./data/1440/1.5.js')));
 app.get('/1440/1.6', (req, res) => res.render(resolve(UI_SRC_DIR, TEMPLATES_DIR, './1440/1.6.pug'), require('./data/1440/1.6.js')));
 app.get('/1440/2.1', (req, res) => res.render(resolve(UI_SRC_DIR, TEMPLATES_DIR, './1440/2.1.pug'), require('./data/1440/2.1.js')));
+app.get('/1440/3.1', (req, res) => res.render(resolve(UI_SRC_DIR, TEMPLATES_DIR, './1440/3.1.pug'), require('./data/1440/3.1.js')));
 
 app.listen(8000);
