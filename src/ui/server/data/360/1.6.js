3030ca0e250ff48a39cf536f589554b5667a2c66
Muxa89 <muxa.89@inbox.ru>
2019-09-22 19:54:59 +0400

    Finished page 1.6 for 360 px width.

--- src/ui/server/data/360/1.6.js
+++ src/ui/server/data/360/1.6.js
@@ -1,76 +1,33 @@
 module.exports = {
-  breadcrumbsList: ['root', 'trunk', 'arcadia', 'addapter', 'ya.make'],
-  item: 'ya.make',
+  breadcrumbsList: ['arcadia', 'trunk', 'arcadia', 'arcanum'],
+  item: 'arcanum',
   branch: 'trunk',
   lastCommit: {
-    hash: 'l32s23',
+    hash: 'c4d248',
     date: '20 Oct 2017, 12:24',
     commiter: 'robot-srch-releaser'
   },
   viewSelectorList: [{ name: 'DETAILS', selected: false }, { name: 'HISTORY', selected: true }],
   history: [{
-      date: 'Today',
-      commits: [{
-        title: 'Support new distbuild protocol',
-        comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
-        hash: 's324e8',
-        commiter: 'woodcutter',
-        time: '4 s ago'
-      }, {
-        title: 'Force executing on MULTISLOT hosts by declaring Caches',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'rudskoy',
-        time: '1 min ago'
-      }, {
-        title: 'Fixed run_diff_test in the realm',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'annaveronika',
-        time: 'at 16:25'
-      }]
+    date: 'Today',
+    commits: [{
+      title: 'Support new distbuild protocol',
+      comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
+      hash: 's324e8',
+      commiter: 'woodcutter',
+      time: '4 s ago'
     }, {
-      date: 'Yesterday',
-      commits: [{
-        title: 'Support HTTP resources',
-        comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
-        hash: 's324e8',
-        commiter: 'woodcutter',
-        time: '4 s ago'
-      }, {
-        title: 'Fixed run_diff_test',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'rudskoy',
-        time: '1 min ago'
-      }, {
-        title: 'Update ya-bin and test_tool',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'annaveronika',
-        time: 'at 16:25'
-      }]
+      title: 'Support new distbuild protocol',
+      comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
+      hash: 's324e8',
+      commiter: 'woodcutter',
+      time: '4 s ago'
     }, {
-      date: 'Jan 11',
-      commits: [{
-        title: 'Support HTTP resources',
-        comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
-        hash: 's324e8',
-        commiter: 'woodcutter',
-        time: '4 s ago'
-      }, {
-        title: 'Fixed run_diff_test',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'rudskoy',
-        time: '1 min ago'
-      }, {
-        title: 'Update ya-bin and test_tool',
-        comment: 'Не очень правильно с точки зрения ООП написаны некоторые функции. Предлагаю исправить это. ',
-        hash: 's324e8',
-        commiter: 'annaveronika',
-        time: 'at 16:25'
-      }]
-    }
-  ]
+      title: 'Support new distbuild protocol',
+      comment: 'Не очень качественно написаны некоторые функции. Предлагаю исправить это недоразумение.',
+      hash: 's324e8',
+      commiter: 'woodcutter',
+      time: '4 s ago'
+    }]
+  }]
 }
