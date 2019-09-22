327e8a97c164c32c8a05eab469b19cc639157cf1
Muxa89 <muxa.89@inbox.ru>
2019-09-22 21:40:14 +0400

    Finished page 2.1 for 360 px width.

--- src/ui/server/data/360/2.1.js
+++ src/ui/server/data/360/2.1.js
@@ -10,16 +10,10 @@ module.exports = {
   viewSelectorList: [{ name: 'FILES', selected: false }, { name: 'BRANCHES', selected: true }],
   branches: [
     { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-2', hash: '7748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-3', hash: '8748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-5', hash: '9748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-6', hash: '1748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-7', hash: '2748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-8', hash: '3748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-9', hash: '4748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-10', hash: '5748ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-11', hash: '4548ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-12', hash: '3248ds893432438dsd823329d923482d' },
-    { name: 'users/a-aidyn00/my-feature-13', hash: '1348ds893432438dsd823329d923482d' }
+    { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' },
+    { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' },
+    { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' },
+    { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' },
+    { name: 'trunk', hash: '6748ds893432438dsd823329d923482d' }
   ]
 }
