fed0a3ea5bd1c62f0ebd262b510b1645766bf982
Muxa89 <muxa.89@inbox.ru>
2019-09-24 16:21:58 +0400

    Fixed my promise realisation. Added polyfill. Fixed promise check.

--- src/promise.js
+++ src/promise.js
@@ -1,72 +1,111 @@
-function MyPromise(executor) {
-  var onFulfill = undefined;
-  var onReject = undefined;
+(function () {
+  function MyPromise(executor) {
+    this.onFulfill = undefined;
+    this.onReject = undefined;
 
-  var resolve = function (value) {
-    if (onFulfill !== undefined) {
-      onFulfill(value);
+    var thisPromise = this;
+
+    this.resolve = function (value) {
+      if (this.onFulfill !== undefined) {
+        this.onFulfill(value);
+      }
     }
-  };
 
-  var reject = function (error) {
-    if (onReject !== undefined) {
-      onReject(error);
+    this.reject = function (error) {
+      if (this.onReject !== undefined) {
+        this.onReject(error);
+      }
     }
-  };
 
-  setTimeout(function () {
-    try {
-      executor(resolve, reject);
-    } catch (err) {
-      reject(err);
-    }
-  }, 0);
+    setTimeout(function () {
+      try {
+        if (executor) {
+          executor.bind(thisPromise)(thisPromise.resolve.bind(thisPromise), thisPromise.reject.bind(thisPromise));
+        }
+      } catch (err) {
+        thisPromise.reject(err);
+      }
+    }, 0)
 
-  var thenFunction = function (success, failure) {
-    return new MyPromise(function (resolve, reject) {
-      onFulfill = function (value) {
-        resolve(success(value));
+
+    this.then = function (successFn, failureFn) {
+      var res = new MyPromise();
+
+      this.onFulfill = function (value) {
+        var successRes = successFn(value);
+        if (successRes && successRes.then && typeof successRes.then === 'function') {
+          successRes.then(function (succValue) {
+            try {
+              res.resolve(succValue);
+            } catch (err) {
+              res.reject(err);
+            }
+          });
+        } else {
+          res.resolve(successRes);
+        }
       };
-      onReject = function (err) {
-        reject(failure(err));
-      };
-    });
-  };
 
-  var catchFunction = function (failure) {
-    return thenFunction(undefined, failure);
+      this.onReject = function (err) {
+        if (failureFn) {
+          var failureRes = failureFn(err);
+          res.resolve(failureRes);
+        } else {
+          res.reject(err);
+        }
+      };
+
+      return res;
+    };
+
+    this.catch = function (failure) {
+      return this.then(undefined, failure);
+    }
   }
 
-  return {
-    then: thenFunction,
-    catch: catchFunction
-  };
-}
+  var globalObject;
+  if (typeof window !== 'undefined') {
+    globalObject = window;
+  } else if (typeof global !== 'undefined') {
+    globalObject = global;
+  }
+  if (!globalObject) return;
+
+  if (!globalObject.Promise) {
+    globalObject.Promise = MyPromise;
+  }
+
+  globalObject.MyPromise = MyPromise;
+})();
 
 // Проверка работоспособности
-var promise = new Promise(function (resolve) {
-  resolve(42)
-})
+var promise = new MyPromise(function (resolve) {
+  setTimeout(resolve, 1000, 42);
+});
 
 promise
   .then(function (value) {
-    return value + 1
+    console.log(value); //42
+    return value + 1;
   })
   .then(function (value) {
-    console.log(value) // 43
+    console.log(value); // 43
     return new Promise(function (resolve) { resolve(137) })
   })
   .then(function (value) {
-    console.log(value) // 137
+    console.log(value); // 137
+    return new MyPromise(function (resolve) { resolve(444) })
+  })
+  .then(function (value) {
+    console.log(value) // 444
     throw new Error("Error Message")
   })
   .then(function () {
     console.log('Будет проигнорировано')
   })
   .catch(function (err) {
-    console.log(err.message);
+    console.log('catched error: ' + err.message);
   })
   .then(function () {
-    console.log('123');
-  })
-
+    console.log('123'); //123
+  });
